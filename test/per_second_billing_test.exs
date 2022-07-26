3d10952b51a3a50d5770b74976a0e429b2922c69
Kerry Buckley <kerryjbuckley@gmail.com>
2022-07-26 16:21:58 +0100

    Hard-code empty result for empty input

--- test/per_second_billing_test.exs
+++ test/per_second_billing_test.exs
@@ -1,3 +1,11 @@
 defmodule PerSecondBillingTest do
   use ExUnit.Case
+
+  describe "PerSecondBilling.report/1" do
+    test "produces an empty report if no time has been booked" do
+      assert PerSecondBilling.report([]) == """
+             DATE, HOUR_0, HOUR_1, HOUR_2, HOUR_3, HOUR_4, HOUR_5, HOUR_6, HOUR_7, HOUR_8, HOUR_9, HOUR_10, HOUR_11, HOUR_12, HOUR_13, HOUR_14, HOUR_15, HOUR_16, HOUR_17, HOUR_18, HOUR_19, HOUR_20, HOUR_21, HOUR_22, HOUR_23
+             """
+    end
+  end
 end
