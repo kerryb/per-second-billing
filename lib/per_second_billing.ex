eb2f8bf91dc62fd1c6fce10ab5e04946200b1132
Kerry Buckley <kerryjbuckley@gmail.com>
2022-07-27 18:35:00 +0100

    Refactor header/1 to remove duplication

--- lib/per_second_billing.ex
+++ lib/per_second_billing.ex
@@ -11,7 +11,7 @@ defmodule PerSecondBilling do
   end
 
   defp header do
-    "DATE, HOUR_0, HOUR_1, HOUR_2, HOUR_3, HOUR_4, HOUR_5, HOUR_6, HOUR_7, HOUR_8, HOUR_9, HOUR_10, HOUR_11, HOUR_12, HOUR_13, HOUR_14, HOUR_15, HOUR_16, HOUR_17, HOUR_18, HOUR_19, HOUR_20, HOUR_21, HOUR_22, HOUR_23\n"
+    "DATE, #{Enum.map_join(0..23, ", ", &"HOUR_#{&1}")}\n"
   end
 
   defp parse_period(period) do
